a7f4d9fa81b928f1b2930e45283bd48a5e11dd63
vnboottop-byte <vnboot.top@vnboot.top>
2026-05-09 05:37:02 +0700

    Auto Sync: 05:37:02

--- js/phai_bansung.js
+++ js/phai_bansung.js
@@ -656,7 +656,7 @@
                                 
                                 
                                 
-                                window.vuKhiModel.rotation.set(Math.PI / 1, Math.PI / -2, 0);
+                                window.vuKhiModel.rotation.set(Math.PI / 3, Math.PI / -2, 0);
 
                             } else {
                                 playerModel.add(window.vuKhiModel);
