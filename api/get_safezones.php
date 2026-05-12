7fda9db8ea1903cc1da4127bfcfab7a381e4fbbc
vnboottop-byte <vnboot.top@vnboot.top>
2026-05-12 13:34:02 +0700

    Auto Sync: 13:34:02

--- api/get_safezones.php
+++ api/get_safezones.php
@@ -1,7 +1,13 @@
 <?php
 header('Content-Type: application/json');
 require_once '../db.php';
-$res = $conn->query("SELECT * FROM safe_zones");
+
+$zone = isset($_GET['zone']) ? $_GET['zone'] : 'TRUNG_CHAU';
+$stmt = $conn->prepare("SELECT * FROM safe_zones WHERE zone_id = ?");
+$stmt->bind_param("s", $zone);
+$stmt->execute();
+$res = $stmt->get_result();
+
 $data = [];
 if ($res) { while ($row = $res->fetch_assoc()) { $data[] = $row; } }
 echo json_encode(['status' => 'success', 'data' => $data]);
--- api/get_teleports.php
+++ api/get_teleports.php
@@ -1,7 +1,13 @@
 <?php
 header('Content-Type: application/json');
 require_once '../db.php';
-$res = $conn->query("SELECT * FROM truyen_tong_tran");
+
+$zone = isset($_GET['zone']) ? $_GET['zone'] : 'TRUNG_CHAU';
+$stmt = $conn->prepare("SELECT * FROM truyen_tong_tran WHERE zone_id = ?");
+$stmt->bind_param("s", $zone);
+$stmt->execute();
+$res = $stmt->get_result();
+
 $data = [];
 if ($res) { while ($row = $res->fetch_assoc()) { $data[] = $row; } }
 echo json_encode(['status' => 'success', 'data' => $data]);
