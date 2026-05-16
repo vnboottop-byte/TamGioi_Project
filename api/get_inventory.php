5aa4eb021d89c3d67a8205b7100015b02f25c248
vnboottop-byte <vnboot.top@vnboot.top>
2026-05-16 09:39:01 +0700

    Auto Sync: 09:39:01

--- api/get_inventory.php
+++ api/get_inventory.php
@@ -11,7 +11,7 @@ $stmt_gold = $conn->query("SELECT game_gold FROM game_characters WHERE username 
 $game_gold = ($stmt_gold && $stmt_gold->num_rows > 0) ? $stmt_gold->fetch_assoc()['game_gold'] : 0;
 
 // Lấy đồ đạc kèm theo Chỉ số và Yêu cầu Phái
-$sql = "SELECT inv.id as inv_id, inv.item_id, inv.item_type, inv.is_equipped, 
+$sql = "SELECT inv.id as inv_id, inv.item_id, inv.item_type, inv.is_equipped, inv.upgrade_level,
                shop.name, shop.model_url, shop.required_class, shop.bonus_damage, shop.bonus_hp, shop.price 
         FROM user_inventory inv 
         JOIN shop_items shop ON inv.item_id = shop.id 
