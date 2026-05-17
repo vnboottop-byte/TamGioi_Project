b9f699842fdf6a865794bbe070d190be3dd26919
vnboottop-byte <vnboot.top@vnboot.top>
2026-05-17 20:38:02 +0700

    Auto Sync: 20:38:02

--- js/ui_logic.js
+++ js/ui_logic.js
@@ -514,7 +514,6 @@ function hienThi3DTrongTui(url, loaiDo, capDo = 0) {
 
 
 
-
 window.thucHienHanhDongTrangBi = function(invId, action) {
     if (action === 'equip') {
         let item = window.khoDoData.find(i => i.inv_id == invId);
@@ -524,16 +523,42 @@ window.thucHienHanhDongTrangBi = function(invId, action) {
         }
     }
 
+    // Gửi yêu cầu mặc/tháo lên Server
     fetch('api/toggle_equip.php', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ inv_id: invId, action: action })
     }).then(res => res.json()).then(data => {
         if(data.status === 'success') {
-            location.reload();
-        } else window.hienThongBaoGame("Lỗi thay đồ!", false);
+            
+            // 🌟 ĐÃ XÓA LỆNH location.reload(); LÀM ĐEN MÀN HÌNH!
+            // THAY BẰNG: Gọi API lấy lại dữ liệu túi đồ ngầm dưới nền
+            fetch('api/get_inventory.php')
+            .then(res2 => res2.json())
+            .then(data2 => {
+                if(data2.status === 'success') {
+                    // Cập nhật lại kho dữ liệu trên RAM
+                    window.khoDoData = data2.data; 
+                    
+                    // Vẽ lại các ô lưới (Để hiện/tắt tag [MẶC])
+                    window.renderTrangTuiDo(); 
+                    
+                    // Vẽ lại bảng thông tin bên phải (Để đổi nút Mặc thành Tháo)
+                    let itemMoi = window.khoDoData.find(i => i.inv_id == invId);
+                    if (itemMoi) window.chonXemMonDo(itemMoi); 
+                    
+                    window.hienThongBaoGame("✔️ Thao tác trang bị thành công!", true);
+                }
+            });
+
+        } else {
+            window.hienThongBaoGame("Lỗi thay đồ!", false);
+        }
     });
 };
+
+
+
 
 
 // 🌟 THUẬT TOÁN BÁN RÁC (PHÂN RÃ)
