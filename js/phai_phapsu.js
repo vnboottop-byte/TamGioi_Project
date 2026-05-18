ac187a71f235c256d77b4d9845877a9477640516
vnboottop-byte <vnboot.top@vnboot.top>
2026-05-18 20:59:02 +0700

    Auto Sync: 20:59:02

--- js/phai_phapsu.js
+++ js/phai_phapsu.js
@@ -16,6 +16,7 @@
 
     window.tongSoChuNoi_PS = 0;
     function taoSoSatThuongPS(pos3D, satThuong, mauSac = '#00ffff') {
+        if (window.isMobile) return; // 🌟 CỨU SỐNG CPU MOBILE!
         if (satThuong <= 0) return;
         // 🌟 KHÓA VAN MOBILE
         if (window.isMobile && window.tongSoChuNoi_PS > 5) return;
