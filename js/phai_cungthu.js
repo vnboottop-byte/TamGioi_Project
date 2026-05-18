e928e3209c9e3e988dd363d7b98b1de7cc2283da
vnboottop-byte <vnboot.top@vnboot.top>
2026-05-18 21:00:01 +0700

    Auto Sync: 21:00:01

--- js/phai_cungthu.js
+++ js/phai_cungthu.js
@@ -43,6 +43,7 @@
     // ==========================================
     window.tongSoChuNoi_CT = 0;
     function taoSoSatThuongCT(pos3D, satThuong, mauSac = '#ff2222') {
+        if (window.isMobile) return; // 🌟 CỨU SỐNG CPU MOBILE!
         if(satThuong <= 0) return;
         // 🌟 KHÓA VAN MOBILE
         if (window.isMobile && window.tongSoChuNoi_CT > 5) return;
