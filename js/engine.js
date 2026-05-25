6dd89dab207e2b957436b316f57ad5f7504860e6
vnboottop-byte <vnboot.top@vnboot.top>
2026-05-25 22:55:02 +0700

    Auto Sync: 22:55:02

--- js/engine.js
+++ js/engine.js
@@ -1953,7 +1953,7 @@ function animate() {
 
 
 
-                var currentWalk = 0.15; var currentSprint = 0.2; var tangKhongGian = "VŨ TRỤ"; var mauChu = "#ff00ff"; var isFlying = doCao > 5.0;
+                var currentWalk = 0.15; var currentSprint = 0.4; var tangKhongGian = "VŨ TRỤ"; var mauChu = "#ff00ff"; var isFlying = doCao > 5.0;
 
                 if (doCao <= 5.0 && timThayDat) { currentWalk = 0.15; currentSprint = 0.4; tangKhongGian = "🌍 MẶT ĐẤT"; mauChu = "#00ff00"; }
                 else if (doCao < 1000.0 && timThayDat) { currentWalk = 0.5; currentSprint = 0.8; tangKhongGian = "⚔️ BẦU KHÍ QUYỂN"; mauChu = "#ffff00"; }
