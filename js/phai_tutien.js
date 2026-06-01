d7cee4cbb903ad475a9cbe40b53387e4c17f3035
vnboottop-byte <vnboot.top@vnboot.top>
2026-06-02 02:50:02 +0700

    Auto Sync: 02:50:02

--- js/phai_tutien.js
+++ js/phai_tutien.js
@@ -11,7 +11,7 @@ let isCuoiKiemSetup = false;
 
 
 window.layMucTieuGanNhatTT = function(viTriGoc, huongMat) {
-    let targetPos = null; let minD = 500;
+    let targetPos = null; let minD = 80;
     if (typeof remotePlayers !== 'undefined') {
         for (let id in remotePlayers) {
             let rp = remotePlayers[id];
